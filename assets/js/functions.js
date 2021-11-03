10e556a8d86eb47acc23570a4b9995aa4139b6aa
Me-Alex <60211801+Me-Alex@users.noreply.github.com>
2021-11-03 12:27:07 +0200

    Added -> user can register

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,21 @@
-function hide(parameter){
-    const findParameter=document.querySelector('.'+parameter+"");
-    findParameter.style.display="none";
+function hide(parameter) {
+    const findParameter = document.querySelector('.' + parameter + "");
+    findParameter.style.display = "none";
+}
+function findParameter(parameter) {
+    const par = document.querySelector(parameter);
+    return par;
+}
+function validateParameter(parameter){
+    if(parameter!=null){
+        addToDataBase();
+    }
+}
+function getInput(parameter) {
+    const el = findParameter(parameter);
+    
+    return el;
+}
+function addToDataBase(){
+
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
 hide("logged-in");
+// console.log(getInput(".username").value);
+let submitB = findParameter(".submit");
+let data={};
+submitB.onclick = () => {
+    let user = getInput(".username").value;
+    let pass = getInput(".password").value;
+    let email= getInput(".email").value;
+    console.log("aici ");
+    data["user"] = user;
+    data["pass"]= pass;
+    data["email"]=email;
+    // data["id"]=2;
+    firebase.database().ref('test2/'+data["user"]).set(data, function (error) {
+        if (error) {
+            console.log({ error });
+        } else {
+            alert("success");
+        }
+    });
+}
